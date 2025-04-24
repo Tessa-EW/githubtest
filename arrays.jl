0e547bfc6e26715235426f7714a106963ae6709a
Tessa-EW <tessa@elkaim-weil.com>
2025-04-24 17:35:45 +0200

    arrays
    test of implementation

---
+++ arrays.jl
@@ -0,0 +1,27 @@
+alph = Dict("a"=>2, "b"=>2,"c"=>2,
+"d"=>3, "e"=>3,"f"=>3,
+" "=> 0 )
+
+function encode(X::String)
+    for i in X
+        A = alph[string(i)] 
+        println(A)
+    end
+end
+
+
+
+alphinv = Dict(
+    "0"=>" ",
+    "2"=>"abc",
+    "3"=>"def" )
+
+  function Adecode(X)
+    L = ""
+    for i in string(X) 
+        A = alphinv[string(i)]
+        L = L*A
+    end 
+    println(L)   
+  end
+
